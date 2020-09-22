006f41be304e859c27daadf1616326a84c0239e3
David Christoffersen <d@vidc.dk>
2020-09-22 15:10:40 -0400

    ARGH server paths

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 // listen to port 3000
 const { PORT = 3000 } = process.env;
 const app = express();
+const path = require("path");
 
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -9,5 +10,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   // if everything works fine, the console will show which port the application is listening to
-
   })
