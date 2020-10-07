62ca3f82c2039e30fb3c7d931cd1d7abc164a218
David Christoffersen <d@vidc.dk>
2020-10-07 11:40:44 -0400

    Error message implementedc

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const fs = require('fs').promises;
 const router = require('express').Router(); // creating Express Router
 const path = require('path');
 const dataBase = path.join(__dirname, '..', 'data', 'users.json');
+const User = require('../models/user');
+
 
 function getData(dataPath) {
   return fs.readFile(dataPath, { enconding: 'utf-8' })
@@ -29,6 +31,17 @@ router.get('/:id', (req, res) => {
       }
       res.status(404).send({ "message": "User ID not found" });
     });
+});
+
+router.post('/', (req, res) => {
+  const { name, about, avatar } = req.body; // get the name and description of the user
+
+  User.create({ name, about, avatar })
+  .then(user => res.send({data: user}))//Returns the data
+  //in case of errors
+  .catch(err => res.status(500).send({ message: 'What did you do This time ??' }));
+
+
 });
 
 module.exports = router;
