31ad3a3e6f49fe13e184f3c8c83759b7dd76ec96
David Christoffersen <d@vidc.dk>
2020-10-10 10:42:51 -0400

    corrections

--- controllers/cards.js
+++ controllers/cards.js
@@ -13,7 +13,11 @@ module.exports.createCard = (req, res) => {
     })
 
     .catch((error) => {
-      res.status(500).send({ message: 'What did you do This time ??' })
+      if (err.name === "ValidationError") {
+        res.status(400).send({ message: "User validation failed" });
+      } else {
+        res.status(500).send({ message: "Internal server error" });
+      }
     });
 };
 
@@ -22,11 +26,14 @@ module.exports.returnCards = (req, res) => {
     .then((cards) => {
       res.send(cards);
     })
-    .catch(err => res.status(500).send({ message: "An error occurred" }));
-}
+    .catch((err) => {
+      res.status(500).send({ message: "An error occurred" });
+    });
+};
+
 
 module.exports.deleteCard = (req, res) => {
-  let cId = req.params.cardId; //Cardid from request
+  const cId = req.params.cardId; //Cardid from request
   Cards.findByIdAndRemove(cId)
     .then((card) => {
       if (card) {
@@ -41,37 +48,37 @@ module.exports.deleteCard = (req, res) => {
 };
 
 module.exports.likeCard = (req, res) => {
- Cards.findByIdAndUpdate(
+  Cards.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // add _id to the array if it's not there yet
     { new: true },
   )
-  .then((card) => {
-    if(card) {
-      res.status(200).send({data: card});
-    } else {
-      res.status(404).send({message: "Card not found"} );
-    }
-  })
-  .catch((err) => {
-    res.status(500).send({ Haiku: "With searching comes loss . . . The presence of absence . . . an error found" });
-  });
+    .then((card) => {
+      if (card) {
+        res.status(200).send({ data: card });
+      } else {
+        res.status(404).send({ message: "Card not found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ Haiku: "With searching comes loss . . . The presence of absence . . . an error found" });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
   Cards.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } }, // remove _id from the array
-  { new: true },
-)
-  .then((card) => {
-    if(card) {
-      res.status(200).send({data: card});
-    } else {
-      res.status(404).send({message: "Card not found"} );
-    }
-  })
-  .catch((err) => {
-    res.status(500).send({ Haiku: "With searching comes loss . . . The presence of absence . . . an error found" });
-  });
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // remove _id from the array
+    { new: true },
+  )
+    .then((card) => {
+      if (card) {
+        res.status(200).send({ data: card });
+      } else {
+        res.status(404).send({ message: "Card not found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({ Haiku: "With searching comes loss . . . The presence of absence . . . an error found" });
+    });
 };
--- controllers/users.js
+++ controllers/users.js
@@ -5,30 +5,46 @@ module.exports.returnAllUsers = (req, res) => {
     .then((users) => {
       res.send(users)
     })
-    .catch(err => res.status(500).send({ "message": "An error occurred" }));
+    .catch((err) => {
+      res.status(500).send({ "message": "An error occurred" });
+    });
 };
 
 module.exports.createNewUser = (req, res) => {
   const { name, about, avatar } = req.body; // get name avatar description of the user
   User.create({ name, about, avatar })
-.then(user => res.send({data: user}))//Returns the data
-  //in case of errors
-.catch(err => res.status(500).send({"message": "An error occurred"}));
+    .then((user) => {
+      res.send({ data: user });  //Returns the data
+    })
+    //in case of errors
+    .catch((err) => {
+      if (err.name === "ValidationError") {
+        res.status(400).send({ message: "User validation failed" });
+    } else {
+        res.status(500).send({ message: "Internal server error" });
+    }
+
+    });
 }
 
 module.exports.getUserById = (req, res) => {
-  let userId = req.params.id;
+  const userId = req.params.id;
   User.findById(userId)
     .then((user) => {
+      console.log(user);
       if (user) {
         return res.send(user);
       } else {
-        return res.status(404).send({"message": "User Not Found"});
+        return res.status(404).send({ "message": "User Not Found" });
       }
     })
     .catch((err) => {
 
+      if (err.name === "CastError") {
+        res.status(404).send({ "Message": "To be known...is to be found...User unknown"})
+      } else {
       res.status(500).send({ "message": "An error occurred" });
+      }
     })
 };
 
@@ -38,42 +54,42 @@ module.exports.updateUser = (req, res) => {
     name: req.body.name,
     about: req.body.about,
   })
-  .then((user) => {
-    if(user) {
-      res.status(200).send({data: user});
-    } else {
-      res.status(404).send({"message": "User not found. Nothing updated"});
-    }
-  })
-  .catch((err) => {
-    if (err._message == 'Validation failed') {
-      res.status(400).send({"error Message": "Data provided failed validation. Please update with valid data"});
-    } else {
-    res.status(500).send({ Haiku: "To have no errors . . . Would be life without meaning . . . No struggle, no joy" });
-    }
-});
+    .then((user) => {
+      if (user) {
+        res.status(200).send({ data: user });
+      } else {
+        res.status(404).send({ "message": "User not found. Nothing updated" });
+      }
+    })
+    .catch((err) => {
+      if (err._message === 'Validation failed') {
+        res.status(400).send({ "error Message": "Data provided failed validation. Please update with valid data" });
+      } else {
+        res.status(500).send({ Haiku: "To have no errors . . . Would be life without meaning . . . No struggle, no joy" });
+      }
+    });
 }
 
 
 
 module.exports.updateAvatar = (req, res) => {
-  const  newAvatar = req.body.avatar;
+  const newAvatar = req.body.avatar;
   User.findByIdAndUpdate(req.user._id, {
     avatar: newAvatar,
   })
-  .then((user) => {
-    if(user) {
-      res.status(200).send({data: user});
-    } else {
-      res.status(404).send({"message": "User not found. Nothing updated"});
-    }
-  })
-  .catch((err) => {
-    if (err._message == 'Validation failed') {
-      res.status(400).send({"error Message": "Validation failed. Please supply a valid URL"});
-    }
-    res.status(500).send({ Haiku: "With searching comes loss . . . The presence of absence . . . an error found" });
-  })
+    .then((user) => {
+      if (user) {
+        res.status(200).send({ data: user });
+      } else {
+        res.status(404).send({ "message": "User not found. Nothing updated" });
+      }
+    })
+    .catch((err) => {
+      if (err._message === 'Validation failed') {
+        res.status(400).send({ "error Message": "Validation failed. Please supply a valid URL" });
+      }
+      res.status(500).send({ Haiku: "With searching comes loss . . . The presence of absence . . . an error found" });
+    })
 
 
 };
